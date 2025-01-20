5330f06f22e26397ad3a71f96cb04b7db476c244
zufarf <mzufarfainan@gmail.com>
2025-01-21 01:18:09 +0700

    refactor middleware and routes for authentication handling

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'cors' => \App\Http\Middleware\Cors::class,
-        'isLogin' => \App\Http\Middleware\isLogin::class, 
-        'isTamu' => \App\Http\Middleware\isTamu::class, 
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -27,43 +27,49 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [SessionController::class, 'index'])->middleware('isTamu');
-Route::get('/sesi', [SessionController::class, 'index'])->middleware('isTamu');
-Route::get('/sesi/logout', [SessionController::class, 'logout']);
-Route::post('/sesi/login', [SessionController::class, 'login'])->middleware('isTamu');
 
-Route::resource('/dashboard', DashboardController::class)->middleware('isLogin');
-Route::resource('/dashboardkepala', KepalaController::class,)->middleware('isLogin');
-Route::resource('/barang', BarangController::class)->middleware('isLogin');
-Route::resource('/barangkepala', BrgKepalaController::class)->middleware('isLogin');
-Route::delete('barang/destroy/{id}', [BarangController::class, 'destroy'])->name('barang.destroy');
-Route::resource('/peminjaman', PeminjamanController::class)->middleware('isLogin');
-Route::resource('/pengembalian', PengembalianController::class)->middleware('isLogin');
-//tambahan
-Route::resource('/pengajuan', PengajuanController::class)->middleware('isLogin');
-Route::resource('/pengajuankepala', PgjKepalaController::class)->middleware('isLogin');
-Route::delete('/pengajuan/destroy/{id}', [PengajuanController::class, 'destroy'])->name('pengajuan.destroy');
-Route::patch('/pengajuan/update-status/{id}', [PgjKepalaController::class, 'updateStatus'])->name('pengajuan.updateStatus');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [SessionController::class, 'index']);
+    Route::get('/sesi', [SessionController::class, 'index'])->name('login');
+    Route::post('/sesi/login', [SessionController::class, 'login']);
+});
 
 
+Route::middleware('auth')->group(function () {
+    Route::get('/sesi/logout', [SessionController::class, 'logout']);
 
+    Route::redirect('/home', '/dashboard');
+    Route::resource('/dashboard', DashboardController::class);
+    Route::resource('/dashboardkepala', KepalaController::class,);
+    Route::resource('/barang', BarangController::class);
+    Route::resource('/barangkepala', BrgKepalaController::class);
+    Route::delete('barang/destroy/{id}', [BarangController::class, 'destroy'])
+        ->name('barang.destroy');
+    Route::resource('/peminjaman', PeminjamanController::class);
+    Route::resource('/pengembalian', PengembalianController::class);
 
+    // tambahan
+    Route::resource('/pengajuan', PengajuanController::class);
+    Route::resource('/pengajuankepala', PgjKepalaController::class);
+    Route::delete('/pengajuan/destroy/{id}', [PengajuanController::class, 'destroy'])
+        ->name('pengajuan.destroy');
+    Route::patch('/pengajuan/update-status/{id}', [PgjKepalaController::class, 'updateStatus'])
+        ->name('pengajuan.updateStatus');
 
+    Route::get('/peminjam/register', [PeminjamController::class, 'create']);
+    Route::post('/peminjam/store', [PeminjamController::class, 'store']);
+    Route::resource('/peminjam', PeminjamController::class);
 
-Route::get('/peminjam/register',[PeminjamController::class, 'create'])->middleware('isLogin');
-Route::post('/peminjam/store',[PeminjamController::class, 'store'])->middleware('isLogin');
-Route::resource('/peminjam', PeminjamController::class)->middleware('isLogin');
+    // tambahan cetak
+    Route::get('/print-barang', [CetakController::class, 'cetakBarang'])
+        ->name('cetakBarang');
+    Route::get('/print-peminjaman', [CetakController::class, 'cetakPeminjaman'])
+        ->name('cetakPeminjaman');
+    Route::get('/print-pengajuan', [CetakController::class, 'cetakPengajuan'])
+        ->name('cetakPengajuan');
 
-//tambahan cetak
-Route::get('/print-barang', [CetakController::class, 'cetakBarang'])->middleware('isLogin')->name('cetakBarang');
-Route::get('/print-peminjaman', [CetakController::class, 'cetakPeminjaman'])->middleware('isLogin')->name('cetakPeminjaman');
-Route::get('/print-pengajuan', [CetakController::class, 'cetakPengajuan'])->middleware('isLogin')->name('cetakPengajuan');
-
-//pengembalian
-Route::delete('/pengembalian/destroy/{id}', [PengembalianController::class, 'destroy'])->name('pengembalian.destroy');
-Route::post('/barang', [BarangController::class, 'store']);
-
-
-
-
-
+    // pengembalian
+    Route::delete('/pengembalian/destroy/{id}', [PengembalianController::class, 'destroy'])
+        ->name('pengembalian.destroy');
+    Route::post('/barang', [BarangController::class, 'store']);
+});
